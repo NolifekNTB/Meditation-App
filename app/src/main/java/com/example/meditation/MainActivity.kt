f338273edb6df186d162db66b36f9a755798c042
NolifekNTB <kacperkotlowskikk1@gmail.com>
2023-06-21 20:31:24 +0200

    First commit

---
+++ app/src/main/java/com/example/meditation/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.meditation
+
+import android.graphics.Typeface
+import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.StyleSpan
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
